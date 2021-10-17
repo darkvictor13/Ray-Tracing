8ca6b768323fbe9199cde62d63de3720e7e8e8c4
darkvictor13 <01victoralmeida@gmail.com>
2021-10-17 17:47:55 -0300

    Capitulos 2 e 3

---
+++ in_one_weekend/src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cctype>
+#include <fstream>
+
+#define IMG_PATH          "../images/"
+#define DEFAULT_FILE_NAME "first.ppm"
+
+#define IMG_WIDTH  256
+#define IMG_HEIGHT 256
+
+int main (int argc, char *argv[]) {
+    std::string file_name;
+    if (argc == 2) {
+        file_name = argv[1];
+    } else {
+        char input;
+        std::cout << "Deseja inserir o nome do arquivo? [s/N] ";
+        std::cin.get(input);
+        if (std::tolower(input) == 's') {
+            std::cout << "Entre com o nome do arquivo: ";
+            std::cin >> file_name;
+        }else{
+            file_name = DEFAULT_FILE_NAME;
+        }
+    }
+
+    std::ofstream file;
+    file.open(IMG_PATH + file_name);
+    file << "P3\n" << IMG_WIDTH << ' ' << IMG_HEIGHT << "\n255\n";
+
+    int i, j;
+    for (j = IMG_HEIGHT - 1; j >= 0; j--) {
+        std::cout << "Faltam " << j << " linhas para gerar\n";
+        for (i = 0; i < IMG_WIDTH; i++) {
+            file << static_cast<int>(255.999 * (double(i) / (IMG_WIDTH - 1)))  << ' ' <<
+                    static_cast<int>(255.999 * (double(j) / (IMG_HEIGHT - 1))) << ' ' <<
+                    static_cast<int>(255.999 * 0.25) << '\n';
+        }
+    }
+
+    file.close();
+    return 0;
+}
